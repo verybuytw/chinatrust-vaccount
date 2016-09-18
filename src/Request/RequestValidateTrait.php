f328fdb959fa4730b0b02e5be185f4758ae8897d
Hughes <hughes@pht-studio.com>
2016-09-18 20:02:39 +0800

    modify

--- src/Request/RequestValidateTrait.php
+++ src/Request/RequestValidateTrait.php
@@ -10,14 +10,28 @@ trait RequestValidateTrait
     /**
      * @param array $params
      *
-     * @return RequestBuilder|InvalidArgumentException
+     * @return mixed RequestBuilder|InvalidArgumentException
      */
     protected function validateParams(array $params)
     {
-        Collection::make(['vaccount', 'amount', 'expired_at'])->each(function($value) use($params) {
-            if (!array_key_exists($value, $params)) {
+        return $this->validateFromRequired(
+            $params,
+            ['vaccount', 'amount', 'expired_at']
+        );
+    }
+
+    /**
+     * @param array $params
+     * @param array $required
+     *
+     * @return mixed RequestBuilder|InvalidArgumentException
+     */
+    protected function validateFromRequired(array $params, array $required = [])
+    {
+        Collection::make($required)->each(function ($value) use ($params) {
+            if (! array_key_exists($value, $params)) {
                 throw new InvalidArgumentException(strtr('{field} is required.', [
-                    '{field}' => $value
+                    '{field}' => $value,
                 ]));
             }
         });
